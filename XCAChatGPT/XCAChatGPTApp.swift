b4d71a6fc326da16d287b38734724abb4573d689
KhanhNguyen <duykhanh.nguyen@unifiedpost.com>
2023-05-02 11:07:14 +0700

    Initial Commit

---
+++ XCAChatGPT/XCAChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCAChatGPTApp.swift
+//  XCAChatGPT
+//
+//  Created by Khanh Nguyen on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct XCAChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
